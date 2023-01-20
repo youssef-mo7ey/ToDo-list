35c525d186de35bf225460149e84049f7560069b
youssef-mo7ey <100730409+youssef-mo7ey@users.noreply.github.com>
2023-01-20 17:26:13 +0200

    new update

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:youssef-mo7ey/ToDO-list.git main:gh-pages
+# git push -f git@github.com:youssef-mo7ey/ToDo-list.git main:gh-pages
 
 cd -
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'C:\Users\ATR\Desktop\mini projects\react.js\vite1\vite-project-1'
+  base:'/vite-project-1/'
 })
